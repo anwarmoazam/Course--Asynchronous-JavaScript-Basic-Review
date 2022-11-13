f31fa125a189c2f79a6fa33cf4651dfccd46f3b3
anwarmoazam <anwar.palimarwar@gmail.com>
2022-11-13 11:35:41 +0530

    practice

---
+++ lesson_1.js
@@ -0,0 +1,9 @@
+console.log('Course--Asynchronous-JavaScript-Basic-Review');
+
+console.log(1);
+console.log(2);
+setTimeout(()=>{
+    console.log('Hello')
+},2000)
+console.log(3);
+console.log(4);
